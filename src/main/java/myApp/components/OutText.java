342fd360d2049a93a67591a633ef366aef5da9da
xaptmah <xaptmah2010@gmail.com>
2021-05-13 15:41:45 +0200

    ref

--- src/main/java/myApp/components/OutText.java
+++ src/main/java/myApp/components/OutText.java
@@ -11,8 +11,7 @@ public class OutText {
 
 
     public String hell() throws IOException {
-        //System.out.println("qwert");
-       // System.out.println(file.exists());
+
         InputStream in = getClass().getResourceAsStream("/hello.txt");
         BufferedReader reader = new BufferedReader(new InputStreamReader(in));
 
@@ -23,9 +22,6 @@ public class OutText {
         }
         in.close();
         reader.close();
-
-        //System.out.println(in);
-
         return hell;
     }
     public static void main(String ... args) throws IOException {
