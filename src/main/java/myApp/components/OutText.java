6fef22243bba0cf244a7bfa77e810278fbe50bcf
xaptmah <xaptmah2010@gmail.com>
2021-05-13 15:32:24 +0200

    read file in web, in tomcat, in PC.

--- src/main/java/myApp/components/OutText.java
+++ src/main/java/myApp/components/OutText.java
@@ -1,16 +1,34 @@
 package myApp.components;
 
-import java.io.File;
+import java.io.*;
+import java.util.Scanner;
 
 public class OutText {
     String hell = "hello";
     String path = "hello.txt";
     File file = new File(path);
+    //Scanner scanner = new Scanner();
 
 
-    public String hell(){
-        System.out.println("qwert");
-        System.out.println(file.exists());
+    public String hell() throws IOException {
+        //System.out.println("qwert");
+       // System.out.println(file.exists());
+        InputStream in = getClass().getResourceAsStream("/hello.txt");
+        BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+
+        String line;
+
+        while ((line = reader.readLine()) !=null){
+            System.out.println(line);
+        }
+        in.close();
+        reader.close();
+
+        //System.out.println(in);
+
         return hell;
+    }
+    public static void main(String ... args) throws IOException {
+        new OutText().hell();
     }
 }
